99ba067b7b83f2406098e1705d4809f2f3e794f9
biedrekGit <128526304+biedrekGit@users.noreply.github.com>
2025-06-23 21:53:08 +0200

    22.1. Correct trelloApiRoot

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://kodilla-tasks-tf22.onrender.com/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const trelloApiRoot = 'ttps://kodilla-tasks-tf22.onrender.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
